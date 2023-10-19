f950fda1452d855d90a3bca0725fda49d1ba1053
KotDev99 <KotDev99@gmail.com>
2023-10-19 22:56:41 +0900

    [Settings] CardStackView Library dependency 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,14 @@ pluginManagement {
 		gradlePluginPortal()
 	}
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
 	repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 	repositories {
 		google()
 		mavenCentral()
+		maven("https://jitpack.io")     // CardStackView Library 에러 해결 부분
 	}
 }
 
