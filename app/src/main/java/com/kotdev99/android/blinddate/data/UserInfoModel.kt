ada599d9125e72245cd4c7eee4cf99dac9796175
KotDev99 <KotDev99@gmail.com>
2023-10-20 04:54:55 +0900

    [Feat] UserInfoModel.kt 생성

---
+++ app/src/main/java/com/kotdev99/android/blinddate/data/UserInfoModel.kt
@@ -0,0 +1,9 @@
+package com.kotdev99.android.blinddate.data
+
+data class UserInfoModel(
+	val uid: String? = null,
+	val nickname: String? = null,
+	val gender: String? = null,
+	val area: String? = null,
+	val age: String? = null
+)
