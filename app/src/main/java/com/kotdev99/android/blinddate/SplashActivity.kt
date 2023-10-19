cf52cd142eaa536543354a88c1398d822eabb96d
KotDev99 <KotDev99@gmail.com>
2023-10-20 02:51:06 +0900

    [Feat] SplashActivity.kt -> 로그인한 사용자인 경우 바로 MainActivity 로 이동

--- app/src/main/java/com/kotdev99/android/blinddate/SplashActivity.kt
+++ app/src/main/java/com/kotdev99/android/blinddate/SplashActivity.kt
@@ -3,13 +3,16 @@ package com.kotdev99.android.blinddate
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.kotdev99.android.blinddate.auth.IntroActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
 	private lateinit var handler: Handler
+	private val auth by lazy { FirebaseAuth.getInstance() }
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -21,10 +24,26 @@ class SplashActivity : AppCompatActivity() {
 	private fun initSplashScreen() {
 
 		handler = Handler(mainLooper)
-		handler.postDelayed({
-			val intent = IntroActivity.newIntent(this)
-			startActivity(intent)
-			finish()
-		}, 2000)
+
+		val uid = auth.currentUser?.uid
+		if (uid != null) {
+			handler.postDelayed({
+				val intent = MainActivity.newIntent(this)
+				startActivity(intent)
+				finish()
+			}, 2000)
+		} else {
+			handler.postDelayed({
+				val intent = IntroActivity.newIntent(this)
+				startActivity(intent)
+				finish()
+			}, 2000)
+		}
+
+		Log.d(TAG, uid.toString())
+	}
+
+	companion object {
+		private val TAG = SplashActivity::class.java.simpleName
 	}
 }
