8a59144c3c78a99f284618c8931cc9c517fc3dea
KotDev99 <KotDev99@gmail.com>
2023-10-19 23:34:48 +0900

    [Feat] MainActivity.kt -> initCardStackView() 함수 작성

--- app/src/main/java/com/kotdev99/android/blinddate/MainActivity.kt
+++ app/src/main/java/com/kotdev99/android/blinddate/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.kotdev99.android.blinddate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.kotdev99.android.blinddate.databinding.ActivityMainBinding
+import com.kotdev99.android.blinddate.slider.CardStackAdapter
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.Direction
 
 class MainActivity : AppCompatActivity() {
+
+	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+	// CardStackView 변수
+	private lateinit var cardStackAdapter: CardStackAdapter
+	private lateinit var manager: CardStackLayoutManager
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		setContentView(binding.root)
+
+		initView()
+		initCardStackView()
+	}
+
+	private fun initView() = with(binding) {
+
+	}
+
+	private fun initCardStackView() {
+
+		manager = CardStackLayoutManager(this, object : CardStackListener {
+			override fun onCardDragging(direction: Direction?, ratio: Float) {
+
+			}
+
+			override fun onCardSwiped(direction: Direction?) {
+
+			}
+
+			override fun onCardRewound() {
+
+			}
+
+			override fun onCardCanceled() {
+
+			}
+
+			override fun onCardAppeared(view: View?, position: Int) {
+
+			}
+
+			override fun onCardDisappeared(view: View?, position: Int) {
+
+			}
+		})
+
+		// 더미 데이터
+		val testList = mutableListOf<String>()
+		testList.apply {
+			add("a")
+			add("b")
+			add("c")
+		}
+
+		cardStackAdapter = CardStackAdapter(this, testList)
+		binding.cardStackView.apply {
+			layoutManager = manager
+			adapter = cardStackAdapter
+		}
 	}
 }
