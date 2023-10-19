8e9dc6a0066e1303b2cfd8c2a7171841be43e748
KotDev99 <KotDev99@gmail.com>
2023-10-20 00:01:14 +0900

    [Settings] SplashActivity.kt, IntroActivity.kt 생성

---
+++ app/src/main/java/com/kotdev99/android/blinddate/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.kotdev99.android.blinddate.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kotdev99.android.blinddate.R
+
+class IntroActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_intro)
+	}
+}
