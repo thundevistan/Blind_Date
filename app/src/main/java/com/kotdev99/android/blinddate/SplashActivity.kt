95b0642f2449af6b715604d4a69aaf61d2d79605
KotDev99 <KotDev99@gmail.com>
2023-10-20 00:17:05 +0900

    [Feat] SplashActivity.kt -> initSplash() 함수 작성

--- app/src/main/java/com/kotdev99/android/blinddate/SplashActivity.kt
+++ app/src/main/java/com/kotdev99/android/blinddate/SplashActivity.kt
@@ -1,11 +1,30 @@
 package com.kotdev99.android.blinddate
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.kotdev99.android.blinddate.auth.IntroActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+	private lateinit var handler: Handler
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_splash)
+
+		initSplashScreen()
+	}
+
+	private fun initSplashScreen() {
+
+		handler = Handler(mainLooper)
+		handler.postDelayed({
+			val intent = IntroActivity.newIntent(this)
+			startActivity(intent)
+			finish()
+		}, 3000)
 	}
 }
--- app/src/main/java/com/kotdev99/android/blinddate/auth/IntroActivity.kt
+++ app/src/main/java/com/kotdev99/android/blinddate/auth/IntroActivity.kt
@@ -1,5 +1,7 @@
 package com.kotdev99.android.blinddate.auth
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kotdev99.android.blinddate.R
@@ -8,5 +10,11 @@ class IntroActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_intro)
+	}
+
+	companion object {
+		fun newIntent(context: Context): Intent {
+			return Intent(context, IntroActivity::class.java)
+		}
 	}
 }
