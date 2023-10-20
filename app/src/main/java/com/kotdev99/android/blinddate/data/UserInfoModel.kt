6b5271885a254623f5b7b375645a9a7da1fba4f8
KotDev99 <KotDev99@gmail.com>
2023-10-20 14:05:44 +0900

    [Fix] UserInfoModel.kt -> @IgnoreExtraProperties 추가

--- app/src/main/java/com/kotdev99/android/blinddate/data/UserInfoModel.kt
+++ app/src/main/java/com/kotdev99/android/blinddate/data/UserInfoModel.kt
@@ -1,5 +1,8 @@
 package com.kotdev99.android.blinddate.data
 
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 data class UserInfoModel(
 	val uid: String? = null,
 	val nickname: String? = null,
